7ac6c3d7d028050148b56c83bdc687f7fefb9d2e
Malek Kchaou <kchaou.malek0@gmail.com>
2023-05-27 12:37:37 -0500

    Added tags to different columns in dataset and coded a program that predicts whether a patient's cancer is malignant or benign using K Nearest Neighbors

--- Regression/StockPrediction.py
+++ Regression/StockPrediction.py
@@ -20,6 +20,7 @@ style.use('ggplot')
 quandl.ApiConfig.api_key = 'zZEPoAwdXYRymNs6tVUe'
 
 df  = quandl.get('WIKI/GOOGL')
+
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close'] * 100
 df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
