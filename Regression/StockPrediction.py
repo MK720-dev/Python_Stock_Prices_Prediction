8293cb5e8f39e3076513f37ca48cd9d51b304f99
Malek Kchaou <kchaou.malek0@gmail.com>
2023-05-28 18:49:50 -0500

    Working on the README and created for Images folder for the screenshots used in the README

--- Regression/StockPrediction.py
+++ Regression/StockPrediction.py
@@ -19,7 +19,7 @@ style.use('ggplot')
 
 quandl.ApiConfig.api_key = 'zZEPoAwdXYRymNs6tVUe'
 
-df  = quandl.get('WIKI/GOOGL')
+df = quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
 df['HL_PCT'] = (df['Adj. High']-df['Adj. Close'])/df['Adj. Close'] * 100
@@ -28,7 +28,7 @@ df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
 #Better feature than these could be chosen for model training 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-#regression feature column
+#regression forecast column
 forecast_col = 'Adj. Close'
 
 #fill in missing data
